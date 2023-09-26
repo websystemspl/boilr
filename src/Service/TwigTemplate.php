857bba38f69e806433a7c479469d5a06aaf49b67
Dariusz Ławiński <genosajd@gmail.com>
2023-09-26 10:49:10 +0200

    update twigtemplate service, fix gitignore

--- src/Service/TwigTemplate.php
+++ src/Service/TwigTemplate.php
@@ -8,15 +8,15 @@ use Websystems\BoilrCore\Interface\TemplateInterface;
 
 class TwigTemplate implements TemplateInterface
 {
-    private $twig;
+    // private $twig;
 
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
+    // public function __construct(Environment $twig)
+    // {
+    //     $this->twig = $twig;
+    // }
 
     public function render(string $view, array $parameters = [])
     {
-        return $this->twig->display($view, $parameters);
+        //return $this->twig->display($view, $parameters);
     }
 }
